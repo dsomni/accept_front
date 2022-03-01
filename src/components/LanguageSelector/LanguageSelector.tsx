f326f52c9e2584a0cee19f381e5071b5ed507d3c
Dmitry <pro100pro10010@gmail.com>
2022-03-01 14:39:37 +0300

    Update [styles] languageSelector

--- src/components/LanguageSelector/LanguageSelector.tsx
+++ src/components/LanguageSelector/LanguageSelector.tsx
@@ -1,46 +1,21 @@
-import { FC, forwardRef, LegacyRef } from 'react';
-import { List, Select } from '@mantine/core';
+import { FC } from 'react';
 import styles from './languageSelector.module.css';
 import { useLocale } from '@hooks/useLocale';
-import Link from 'next/link';
-import { useRouter } from 'next/router';
+import { Select } from '@mantine/core';
 
 export const LanguageSelector: FC = () => {
   const { lang, langList, set } = useLocale();
-  const router = useRouter();
-
-  const CreateSelectItem = (
-    { value }: { value: string },
-    ref: LegacyRef<HTMLDivElement> | undefined
-  ) => (
-    <div ref={ref}>
-      <List.Item className={styles.options} value={value}>
-        <Link href={router.asPath} locale={value}>
-          <a className={styles.link}>{value.toUpperCase()}</a>
-        </Link>
-      </List.Item>
-    </div>
-  );
-  const SelectItem = forwardRef(CreateSelectItem);
-
-  const data = [
-    {
-      value: 'en',
-    },
-    {
-      value: 'ru',
-    },
-  ];
 
   return (
     <>
       <Select
-        className={styles.selector}
+        classNames={{ input: styles.input }}
         defaultValue={lang}
         value={lang}
-        onChange={(e: any) => set(e.target.value)}
-        data={data}
-        itemComponent={SelectItem}
+        onChange={(e: any) => set(e)}
+        data={langList.map((lang) => {
+          return { value: lang, label: lang.toUpperCase() };
+        })}
       />
     </>
   );
