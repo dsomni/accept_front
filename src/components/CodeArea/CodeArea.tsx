b324e12b06ba4ffbd66b2a8674e7d070907d5cc3
RetroMeras <RetroMeras@ya.ru>
2022-03-09 17:01:08 +0300

    Add [load tests on drag, replace fullscrenndropzone on code] components

--- src/components/CodeArea/CodeArea.tsx
+++ src/components/CodeArea/CodeArea.tsx
@@ -4,9 +4,16 @@ import {
   UploadIcon,
   CrossCircledIcon,
 } from '@modulz/radix-icons';
-import { FC, memo, useCallback, useState } from 'react';
+import {
+  FC,
+  memo,
+  useCallback,
+  useEffect,
+  useRef,
+  useState,
+} from 'react';
 import { Textarea } from '@mantine/core';
-import { FullScreenDropzone } from '@mantine/dropzone';
+import { Dropzone, FullScreenDropzone } from '@mantine/dropzone';
 import { useLocale } from '@hooks/useLocale';
 import styles from './codeArea.module.css';
 import { capitalize } from '@utils/capitalize';
@@ -39,6 +46,30 @@ const CodeArea: FC<{
 }> = ({ label, setLanguage, setCode, formProps, classNames }) => {
   const { locale } = useLocale();
 
+  const [drag, setDrag] = useState(false);
+  const dragable = useRef<HTMLDivElement>(null);
+
+  const dragStart = useCallback(() => {
+    setDrag(true);
+  }, []);
+  const dragEnd = useCallback(() => {
+    setDrag(false);
+  }, []);
+
+  useEffect(() => {
+    const current = dragable.current;
+    if (current) {
+      current.addEventListener('dragenter', dragStart);
+      current.addEventListener('dragleave', () => setDrag(false));
+    }
+    return () => {
+      if (current) {
+        current.removeEventListener('dragenter', dragStart);
+        current.removeEventListener('dragleave', dragEnd);
+      }
+    };
+  }, [dragable, dragStart, dragEnd]);
+
   const onDrop = useCallback(
     (files: File[]) => {
       if (files[0]) {
@@ -50,60 +81,62 @@ const CodeArea: FC<{
         }
         files[0].text().then((res) => setCode(res));
       }
+      setDrag(false);
     },
     [setCode, setLanguage]
   );
 
   return (
-    <div>
+    <div ref={dragable}>
       <div className={styles.langSelector}>
         <ProgrammingLangSelector setValue={setLanguage} />
       </div>
-      <Textarea
-        classNames={classNames}
-        placeholder={capitalize(locale.placeholders.code)}
-        onChange={(e) => setCode(e.target.value)}
-        minRows={40}
-        label={label}
-        {...formProps}
-        // value={code}
-      />
-      <FullScreenDropzone
-        disabled={false}
-        accept={['', 'text/x-python', 'text/x-c++src', 'text/rust']}
-        onDrop={(files) => {
-          onDrop(files);
-        }}
+      {!drag && (
+        <Textarea
+          classNames={classNames}
+          placeholder={capitalize(locale.placeholders.code)}
+          onChange={(e) => setCode(e.target.value)}
+          minRows={40}
+          label={label}
+          {...formProps}
+          // value={code}
+        />
+      )}
+      {drag && (
+        <Dropzone
+          disabled={false}
+          accept={['', 'text/x-python', 'text/x-c++src', 'text/rust']}
+          onDrop={onDrop}
+          onReject={() => setDrag(false)}
+        >
+          {(status) => (
+            <Group
+              position="center"
+              spacing="xl"
+              style={{ minHeight: 220, pointerEvents: 'none' }}
+            >
+              <ImageUploadIcon
+                status={status}
+                style={{
+                  width: 80,
+                  height: 80,
+                  color: 'white',
+                }}
+              />
 
-        // onReject={(files) => console.log('rejected files', files[0])}
-      >
-        {(status) => (
-          <Group
-            position="center"
-            spacing="xl"
-            style={{ minHeight: 220, pointerEvents: 'none' }}
-          >
-            <ImageUploadIcon
-              status={status}
-              style={{
-                width: 80,
-                height: 80,
-                color: 'white',
-              }}
-            />
-
-            <div>
-              <Text size="xl" inline>
-                Drag images here or click to select files
-              </Text>
-              <Text size="sm" color="dimmed" inline mt={7}>
-                Attach as many files as you like, each file should not
-                exceed 5mb
-              </Text>
-            </div>
-          </Group>
-        )}
-      </FullScreenDropzone>
+              <div>
+                <Text size="xl" inline>
+                  Drag images here or click to select files
+                </Text>
+                <Text size="sm" color="dimmed" inline mt={7}>
+                  Attach as many files as you like, each file should
+                  not exceed 5mb
+                </Text>
+              </div>
+            </Group>
+          )}
+        </Dropzone>
+      )}
     </div>
   );
 };
--- src/components/Task/Form/Tests/Tests.tsx
+++ src/components/Task/Form/Tests/Tests.tsx
@@ -1,13 +1,52 @@
 import ListItem from '@components/ListItem/ListItem';
+import { ITest } from '@custom-types/ITest';
 import { useLocale } from '@hooks/useLocale';
-import { Button } from '@mantine/core';
+import { Button, Group, Text } from '@mantine/core';
+import { Dropzone } from '@mantine/dropzone';
+import { useEventListener } from '@mantine/hooks';
+import {
+  ImageIcon,
+  UploadIcon,
+  CrossCircledIcon,
+} from '@modulz/radix-icons';
 
 import { capitalize } from '@utils/capitalize';
-import { FC, memo, useCallback } from 'react';
+import {
+  FC,
+  memo,
+  useCallback,
+  useEffect,
+  useRef,
+  useState,
+} from 'react';
 import styles from './tests.module.css';
 
 const Tests: FC<{ form: any }> = ({ form }) => {
   const { locale } = useLocale();
+
+  const [drag, setDrag] = useState(false);
+  const dragable = useRef<HTMLDivElement>(null);
+
+  const dragStart = useCallback(() => {
+    setDrag(true);
+  }, []);
+  const dragEnd = useCallback(() => {
+    setDrag(false);
+  }, []);
+
+  useEffect(() => {
+    const current = dragable.current;
+    if (current) {
+      current.addEventListener('dragenter', dragStart);
+      current.addEventListener('dragleave', () => setDrag(false));
+    }
+    return () => {
+      if (current) {
+        current.removeEventListener('dragenter', dragStart);
+        current.removeEventListener('dragleave', dragEnd);
+      }
+    };
+  }, [dragable, dragStart, dragEnd]);
 
   const onDeleteExample = useCallback(
     (index: number) => {
@@ -22,11 +61,92 @@ const Tests: FC<{ form: any }> = ({ form }) => {
     [form]
   );
 
+  const onDrop = useCallback(
+    async (files: File[]) => {
+      try {
+        if (files === []) return;
+
+        let length = files.length;
+        if (length % 2 !== 0) length++;
+        length = length / 2;
+
+        let tests: ITest[] = new Array(length);
+        for (let i = 0; i < length; i++) {
+          tests[i] = {
+            inputData: '',
+            outputData: '',
+          };
+        }
+        for (let i = 0; i < files.length; i++) {
+          if (files[i].name.startsWith('input')) {
+            const input = await files[i].text();
+            const index = Number(files[i].name.slice('input'.length));
+            if (index >= length) continue;
+            tests[index].inputData = input;
+          } else if (files[i].name.startsWith('output')) {
+            const output = await files[i].text();
+            const index = Number(
+              files[i].name.slice('output'.length)
+            );
+            if (index >= length) continue;
+            tests[index].outputData = output;
+          }
+          // console.log('file');
+        }
+        tests = tests.filter(
+          (test) => test.inputData !== '' && test.outputData !== ''
+        );
+        if (tests !== []) form.setFieldValue('tests', tests);
+      } catch {}
+      setDrag(false);
+    },
+    [form]
+  );
+
   return (
-    <div className={styles.wrapper}>
-      {form.values.tests &&
+    <div ref={dragable} className={styles.wrapper}>
+      {drag && (
+        <Dropzone
+          disabled={false}
+          accept={['*', '']}
+          onDrop={onDrop}
+          onReject={(files) => {
+            console.log(files);
+            setDrag(false);
+          }}
+        >
+          {(status) => (
+            <Group
+              position="center"
+              spacing="xl"
+              style={{ minHeight: 220, pointerEvents: 'none' }}
+            >
+              <ImageUploadIcon
+                status={status}
+                style={{
+                  width: 80,
+                  height: 80,
+                  color: 'white',
+                }}
+              />
+
+              <div>
+                <Text size="xl" inline>
+                  Drag images here or click to select files
+                </Text>
+                <Text size="sm" color="dimmed" inline mt={7}>
+                  Attach as many files as you like, each file should
+                  not exceed 5mb
+                </Text>
+              </div>
+            </Group>
+          )}
+        </Dropzone>
+      )}
+      {!drag &&
+        form.values.tests &&
         form.values.tests.map(
-          (value: [string, string], index: number) => (
+          (_: [string, string], index: number) => (
             <div key={index} className={styles.example}>
               <ListItem
                 field="tests"
@@ -67,3 +187,16 @@ const Tests: FC<{ form: any }> = ({ form }) => {
 };
 
 export default memo(Tests);
+
+function ImageUploadIcon({ ...props }) {
+  const status = props.status;
+  if (status.accepted) {
+    return <UploadIcon {...props} />;
+  }
+
+  if (status.rejected) {
+    return <CrossCircledIcon {...props} />;
+  }
+
+  return <ImageIcon {...props} />;
+}
