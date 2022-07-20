4705178ff2429f6daa34f0bd7120f11c8c716877
Dmitry <pro100pro10010@gmail.com>
2022-07-20 14:34:13 +0300

    Add [helper] ui/numberInput

--- src/components/ui/NumberInput/NumberInput.tsx
+++ src/components/ui/NumberInput/NumberInput.tsx
@@ -1,19 +1,31 @@
-import { FC, memo } from 'react';
+import { FC, memo, ReactNode } from 'react';
 import styles from './numberInput.module.css';
 import {
   NumberInput as MantineNumberInput,
   NumberInputProps,
 } from '@mantine/core';
+import { Helper } from '@ui/Helper/Helper';
 
-const NumberInput: FC<NumberInputProps> = (props) => {
+interface Props extends NumberInputProps {
+  helperContent?: string | ReactNode;
+}
+
+const NumberInput: FC<Props> = (props) => {
   return (
-    <MantineNumberInput
-      classNames={{
-        label: styles.label,
-      }}
-      size="lg"
-      {...props}
-    />
+    <div className={styles.wrapper}>
+      <div className={styles.labelWrapper}>
+        <div className={styles.label}>
+          {props.label}
+          {props.required && (
+            <div className={styles.labelRequired}>*</div>
+          )}
+        </div>
+        {props.helperContent && (
+          <Helper content={props.helperContent} />
+        )}
+      </div>
+      <MantineNumberInput size="lg" {...props} label={undefined} />
+    </div>
   );
 };
 
