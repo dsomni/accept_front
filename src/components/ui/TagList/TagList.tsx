0a3432dc4e9722f1e868bdbf89d1d1d2117087ef
retromeras@ya.ru <retromeras@ya.ru>
2022-07-21 20:48:36 +0300

    Fix [Tags in task.form.preview] ui/TagList

--- src/components/ui/TagList/TagList.tsx
+++ src/components/ui/TagList/TagList.tsx
@@ -1,14 +1,19 @@
 import { ITag } from '@custom-types/data/ITag';
 import { Badge } from '@mantine/core';
-import { FC, memo } from 'react';
+import { FC, memo, useEffect } from 'react';
 import styles from './tagList.module.css';
 
 const TagList: FC<{ tags: ITag[] }> = ({ tags }) => {
+
+  useEffect(() => {
+    console.log(tags)
+  }, [tags])
+
   return (
     <div className={styles.wrapper}>
       {tags.map((tag, idx) => (
         <Badge key={idx} variant="outline" color="gray">
-          {tag.title}
+          {tag.title || tag.label}
         </Badge>
       ))}
     </div>
