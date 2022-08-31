445a2ad2160f3bdd5f39ecd8af5f2276872e5701
Dmitry <pro100pro10010@gmail.com>
2022-08-31 22:49:03 +0300

    Add [working chat] dashboard

--- src/components/Dashboard/Chat/Chat.tsx
+++ src/components/Dashboard/Chat/Chat.tsx
@@ -1,44 +1,81 @@
-import { Button } from '@ui/basics';
-import { getApiUrl } from '@utils/getServerUrl';
-import { FC, memo, useEffect, useState } from 'react';
+import { useUser } from '@hooks/useUser';
+import { Icon, TextInput } from '@ui/basics';
+import { FC, memo, useCallback, useEffect, useState } from 'react';
+import { Send } from 'tabler-icons-react';
 import styles from './chat.module.css';
+import { IMessage } from '@custom-types/data/IMessage';
+import { getLocalDate } from '@utils/datetime';
 
 const Chat: FC<{ spec: string }> = ({ spec }) => {
-  const [messages, setMessages] = useState<string[]>([]);
-  const [send, setSend] = useState<(_: string) => void>(() => {});
+  const [messages, setMessages] = useState<IMessage[]>([]);
+  const [webSocket, setWebSocket] = useState<WebSocket>();
+  const [message, setMessage] = useState('');
+  const { user } = useUser();
+
   useEffect(() => {
+    if (!user) return;
     const ws = new WebSocket(
-      `ws://${process.env.API_ENDPOINT?.split('://')[1]}/chat`,
-      'protocolOne'
+      `ws://${
+        process.env.API_ENDPOINT?.split('://')[1]
+      }/ws/chat/${spec}/${user?.login}`
     );
-    if (ws.readyState == 1) {
-      setSend(ws.send);
-      ws.onmessage = (event) =>
-        setMessages((messages) => {
-          messages.push(event.data);
-          return messages;
-        });
-    }
+
+    ws.onmessage = (event) => {
+      setMessages((messages) => {
+        return [...messages, JSON.parse(event.data)];
+      });
+    };
+
+    setWebSocket(ws);
+
     return () => {
       ws.close();
-      setSend(() => {});
     };
-  }, []);
+  }, [spec, user]);
 
-  useEffect(() => console.log(send), [send]);
+  const handleSend = useCallback(() => {
+    if (
+      webSocket &&
+      webSocket.readyState === 1 &&
+      message.trim().length > 0
+    ) {
+      webSocket.send(message.trim());
+    }
+  }, [webSocket, message]);
 
   return (
     <div className={styles.wrapper}>
-      <Button
-        onClick={() => {
-          if (send) send('text');
-        }}
-      >
-        Send Text
-      </Button>
-      {messages.map((message, index) => (
-        <p key={index}>{message}</p>
-      ))}
+      <div className={styles.messages}>
+        {messages.map((message, index) => (
+          <div
+            className={`${styles.message} ${
+              message.user.login === user?.login ? styles.own : ''
+            }`}
+            key={index}
+          >
+            <div className={styles.user}>
+              {message.user.shortName}
+            </div>
+            <div className={styles.content}>{message.content}</div>
+            <div className={styles.date}>
+              {getLocalDate(message.date)}
+            </div>
+          </div>
+        ))}
+      </div>
+      <div className={styles.sendWrapper}>
+        <TextInput
+          className={styles.input}
+          onChange={(event) => setMessage(event.currentTarget.value)}
+        />
+        <Icon
+          className={styles.button}
+          onClick={handleSend}
+          color="var(--primary)"
+        >
+          <Send />
+        </Icon>
+      </div>
     </div>
   );
 };
