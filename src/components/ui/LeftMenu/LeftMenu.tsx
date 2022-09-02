befb8730f2a2ff1867f0c0d571e41a7e89a48c10
Dmitry <pro100pro10010@gmail.com>
2022-09-02 23:59:07 +0300

    Add [LEftMenu component] components/ui

---
+++ src/components/ui/LeftMenu/LeftMenu.tsx
@@ -0,0 +1,45 @@
+import { IMenuLink } from '@custom-types/ui/IMenuLink';
+import { Group, Navbar, UnstyledButton } from '@mantine/core';
+import { FC, ReactNode, memo, useState } from 'react';
+import styles from './leftMenu.module.css';
+
+const LeftMenu: FC<{
+  links: IMenuLink[];
+  topContent?: ReactNode;
+}> = ({ links, topContent }) => {
+  const [current, setCurrent] = useState(0);
+
+  return (
+    <div className={styles.wrapper}>
+      <Navbar p="xs" width={{ base: 300 }} withBorder={false}>
+        {topContent && <Navbar.Section>{topContent}</Navbar.Section>}
+        <Navbar.Section grow mt="md">
+          {links.map((element, idx) => (
+            <UnstyledButton
+              key={idx}
+              className={`${styles.button} ${
+                current === idx ? styles.activeButton : ''
+              }`}
+              onClick={() => setCurrent(idx)}
+            >
+              <Group spacing="xs" className={styles.groupWrapper}>
+                <div
+                  className={`${styles.line} ${
+                    current === idx ? styles.activeLine : ''
+                  }`}
+                ></div>
+                <Group className={styles.groupWrapper}>
+                  {element.icon}
+                  <div>{element.title}</div>
+                </Group>
+              </Group>
+            </UnstyledButton>
+          ))}
+        </Navbar.Section>
+      </Navbar>
+      <div className={styles.pageWrapper}>{links[current].page}</div>
+    </div>
+  );
+};
+
+export default memo(LeftMenu);
