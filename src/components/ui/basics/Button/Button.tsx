9d7f5b2d7a851292927d88953b4f83cded3de9b9
Dmitry <pro100pro10010@gmail.com>
2022-10-21 17:05:33 +0300

    Improve [button component] ui/basics/Button

--- src/components/ui/basics/Button/Button.tsx
+++ src/components/ui/basics/Button/Button.tsx
@@ -1,4 +1,4 @@
-import { FC, memo, useEffect, useMemo, useState } from 'react';
+import { FC, memo } from 'react';
 import { HoverCard, Button as MantineButton } from '@mantine/core';
 import styles from './button.module.css';
 import { MyButtonProps } from '@custom-types/ui/basics/button';
@@ -8,122 +8,14 @@ const Button: FC<MyButtonProps> = ({
   hoverCardDropdownProps,
   hoverCardTargetProps,
   targetWrapperStyle,
+  targetWrapperClassName,
   buttonWrapperStyle,
   dropdownContent,
   kind,
   variant,
+  shrink,
   ...buttonProps
 }) => {
-  const [bgColor, setBgColor] = useState('#fff)');
-  const [mainColor, setMainColor] = useState('var(--primary)');
-  const [hoverColor, setHoverColor] = useState('var(--secondary');
-  const [fontWeight, setFontWeight] = useState(600);
-
-  useEffect(() => {
-    switch (kind) {
-      case 'positive':
-        switch (variant) {
-          case 'outline':
-            setBgColor('#fff');
-            setMainColor('var(--positive)');
-            setHoverColor('var(--positive-light2)');
-            setFontWeight(400);
-            break;
-          case 'light':
-            setBgColor('var(--positive-light)');
-            setMainColor('var(--positive)');
-            setHoverColor('var(--positive-light)');
-            setFontWeight(600);
-            break;
-          default:
-            setBgColor('var(--positive)');
-            setMainColor('#fff');
-            setHoverColor('var(--positive-light)');
-            setFontWeight(600);
-            break;
-        }
-        break;
-      case 'negative':
-        switch (variant) {
-          case 'outline':
-            setBgColor('#fff');
-            setMainColor('var(--negative)');
-            setHoverColor('var(--negative-light2)');
-            setFontWeight(400);
-            break;
-          case 'light':
-            setBgColor('var(--negative-light)');
-            setMainColor('var(--negative)');
-            setHoverColor('var(--negative-light)');
-            setFontWeight(600);
-            break;
-          default:
-            setBgColor('var(--negative)');
-            setMainColor('#fff');
-            setHoverColor('var(--negative-light)');
-            setFontWeight(600);
-            break;
-        }
-        break;
-      default:
-        switch (variant) {
-          case 'outline':
-            setBgColor('#fff');
-            setMainColor('var(--primary)');
-            setHoverColor('var(--primary-light)');
-            setFontWeight(400);
-            break;
-          case 'light':
-            setBgColor('var(--primary-light)');
-            setMainColor('var(--primary)');
-            setHoverColor('var(--secondary-light)');
-            setFontWeight(600);
-            break;
-          default:
-            setBgColor('var(--primary)');
-            setMainColor('#fff');
-            setHoverColor('var(--secondary)');
-            setFontWeight(600);
-            break;
-        }
-        break;
-    }
-  }, [kind, variant]);
-
-  const button = useMemo(
-    () => (
-      <>
-        <MantineButton
-          styles={{
-            label: {
-              fontSize: 'var(--font-size--btn)',
-              lineHeight: 'var(--font-size-btn-l)',
-              color: buttonProps.disabled
-                ? 'var(--dark2)'
-                : mainColor,
-              fontWeight: fontWeight,
-            },
-            root: {
-              backgroundColor: buttonProps.disabled
-                ? 'var(--dark5) !important'
-                : bgColor,
-              padding: 'var(--spacer-xs) var(--spacer-s)',
-              height: 'fit-content',
-              borderColor: buttonProps.disabled
-                ? 'var(--dark2)'
-                : mainColor,
-              '&:hover': {
-                backgroundColor: hoverColor,
-              },
-            },
-          }}
-          {...buttonProps}
-        />
-      </>
-    ),
-    [bgColor, buttonProps, fontWeight, hoverColor, mainColor]
-  );
-
   return (
     <HoverCard
       withArrow
@@ -133,9 +25,33 @@ const Button: FC<MyButtonProps> = ({
       transitionDuration={300}
       {...hoverCardProps}
     >
-      <div style={targetWrapperStyle}>
+      <div
+        className={targetWrapperClassName}
+        style={targetWrapperStyle}
+      >
         <HoverCard.Target {...hoverCardTargetProps}>
-          <div style={buttonWrapperStyle}>{button}</div>
+          <div
+            style={buttonWrapperStyle}
+            className={
+              `${styles.buttonWrapper} ${
+                shrink ? styles.shrink : ''
+              }` +
+              ' ' +
+              (buttonProps.disabled
+                ? styles.disabled
+                : `${kind && styles[kind]} ${
+                    variant && styles[variant]
+                  }`)
+            }
+          >
+            <MantineButton
+              classNames={{
+                label: styles.label,
+                root: styles.root,
+              }}
+              {...buttonProps}
+            />
+          </div>
         </HoverCard.Target>
       </div>
       {!!dropdownContent && (
