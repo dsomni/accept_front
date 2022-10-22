e6e1a81667169292a4f9a4d6ebe63af99207edfa
Dmitry <pro100pro10010@gmail.com>
2022-10-22 21:41:58 +0300

    Update [NumberInput] components

--- src/components/ui/basics/TextInput/TextInput.tsx
+++ src/components/ui/basics/TextInput/TextInput.tsx
@@ -17,29 +17,29 @@ const TextInput: FC<Props> = ({
   ...props
 }) => {
   return (
-    <div className={shrink ? inputStyles.shrink : ''}>
-      <div className={inputStyles.wrapper}>
-        <div className={inputStyles.labelWrapper}>
-          <div className={inputStyles.label}>
-            {props.label}
-            {props.required && (
-              <div className={inputStyles.labelRequired}>*</div>
-            )}
-          </div>
-          {helperContent && (
-            <Helper dropdownContent={helperContent} />
+    <div
+      className={`${inputStyles.wrapper} ${
+        shrink ? inputStyles.shrink : ''
+      }`}
+    >
+      <div className={inputStyles.labelWrapper}>
+        <div className={inputStyles.label}>
+          {props.label}
+          {props.required && (
+            <div className={inputStyles.labelRequired}>*</div>
           )}
         </div>
-        <MantineInput
-          size={shrink ? 'sm' : 'md'}
-          {...props}
-          classNames={{
-            error: props.classNames?.error || inputStyles.error,
-            ...props.classNames,
-          }}
-          label={undefined}
-        />
+        {helperContent && <Helper dropdownContent={helperContent} />}
       </div>
+      <MantineInput
+        size={shrink ? 'sm' : 'md'}
+        {...props}
+        classNames={{
+          error: props.classNames?.error || inputStyles.error,
+          ...props.classNames,
+        }}
+        label={undefined}
+      />
     </div>
   );
 };
