07e972edc793f940cb322a626780695961c51527
melaveetha <retromeras@ya.ru>
2022-10-30 01:20:06 +0300

    Fix [error page title]

--- pages/404.tsx
+++ pages/404.tsx
@@ -3,12 +3,14 @@ import styles from '@styles/error.module.css';
 import { useLocale } from '@hooks/useLocale';
 
 import Link from 'next/link';
+import Title from '@ui/Title/Title';
 
 const Error: NextPage = () => {
   const { locale } = useLocale();
 
   return (
     <div className={styles.wrapper}>
+      <Title title={'404'} />
       <div className={styles.statusCode}>{404}</div>
       <div className={styles.description}>
         {locale.errorPage.description}
--- pages/500.tsx
+++ pages/500.tsx
@@ -3,12 +3,14 @@ import styles from '@styles/error.module.css';
 import { useLocale } from '@hooks/useLocale';
 
 import Link from 'next/link';
+import Title from '@ui/Title/Title';
 
 const Error: NextPage = () => {
   const { locale } = useLocale();
 
   return (
     <div className={styles.wrapper}>
+      <Title title={'500'} />
       <div className={styles.statusCode}>{500}</div>
       <div className={styles.description}>
         {locale.errorPage.serverError}
