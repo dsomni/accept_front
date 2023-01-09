64e2f193d72b96418bf4c0a637d0d2bf988d2eec
Dmitry <pro100pro10010@gmail.com>
2023-01-09 19:29:18 +0300

    Update [locale] helpers/grade

--- src/locale/en/helpers/grade.ts
+++ src/locale/en/helpers/grade.ts
@@ -7,7 +7,7 @@ export const grade = {
   tableFormat: [
     'Table must have following columns (in any order): login, grade',
     'login - user`s login must be unique; cannot be empty',
-    'grade - group to which user will be added; group must be in such form: `11 A`, cannot be empty',
+    'grade - group to which user will be added; group must be in such form: "11 A", cannot be empty',
     'In case if the grade does not exist, you can create it on the corresponding tab',
     'Old grades will be deleted and replaced by new ones; due to this all the activities connected with grades will become invalid',
   ],
