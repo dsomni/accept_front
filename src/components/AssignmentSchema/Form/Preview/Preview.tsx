04b53f83b0f1174d883086fd0f015a20f04bb5b2
Dmitry <pro100pro10010@gmail.com>
2023-01-19 13:55:51 +0300

    Fix [Preview size] AssignmentSchema, Task, Tournament

--- src/components/AssignmentSchema/Form/Preview/Preview.tsx
+++ src/components/AssignmentSchema/Form/Preview/Preview.tsx
@@ -2,7 +2,11 @@ import Description from '@components/AssignmentSchema/Description/Description';
 import { FC, memo } from 'react';
 
 const Preview: FC<{ form: any }> = ({ form }) => {
-  return <Description assignment={form.values} preview />;
+  return (
+    <div style={{ zoom: '80%' }}>
+      <Description assignment={form.values} preview />
+    </div>
+  );
 };
 
 export default memo(Preview);
