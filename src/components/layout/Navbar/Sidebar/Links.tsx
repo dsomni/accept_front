5b3c7d79506409395abc7199cde31f4b8cf99568
Dmitry <pro100pro10010@gmail.com>
2023-02-21 16:53:57 +0300

    Add [rights check] Sidebar

--- src/components/layout/Navbar/Sidebar/Links.tsx
+++ src/components/layout/Navbar/Sidebar/Links.tsx
@@ -2,40 +2,56 @@ import { FC } from 'react';
 import { IHeaderLink } from '@custom-types/ui/IHeaderLink';
 import { NavLink } from '@mantine/core';
 import { useLocale } from '@hooks/useLocale';
+import { accessLevels } from '@constants/protectedRoutes';
+import { useUser } from '@hooks/useUser';
 
 export const Links: FC<{
   links: IHeaderLink[];
 }> = ({ links }) => {
   const { locale } = useLocale();
 
+  const { accessLevel } = useUser();
+
   return (
     <div>
-      {links.map((link, index) => (
-        <NavLink
-          key={index}
-          label={link.text(locale)}
-          component={link.type !== 'dropdown' ? 'a' : 'button'}
-          href={link.type !== 'dropdown' ? link.href : undefined}
-          opened={true}
-          rightSection={<></>}
-        >
-          {link.links &&
-            link.links.map((sublink, subindex) => (
-              <NavLink
-                key={subindex}
-                label={sublink.text(locale)}
-                component={
-                  sublink.type !== 'dropdown' ? 'a' : 'button'
-                }
-                href={
-                  sublink.type !== 'dropdown'
-                    ? sublink.href
-                    : undefined
-                }
-              />
-            ))}
-        </NavLink>
-      ))}
+      {links
+        .filter(
+          (item) =>
+            !item.permission ||
+            accessLevel >= accessLevels[item.permission]
+        )
+        .map((link, index) => (
+          <NavLink
+            key={index}
+            label={link.text(locale)}
+            component={link.type !== 'dropdown' ? 'a' : 'button'}
+            href={link.type !== 'dropdown' ? link.href : undefined}
+            opened={true}
+            rightSection={<></>}
+          >
+            {link.links &&
+              link.links
+                .filter(
+                  (item) =>
+                    !item.permission ||
+                    accessLevel >= accessLevels[item.permission]
+                )
+                .map((sublink, subindex) => (
+                  <NavLink
+                    key={subindex}
+                    label={sublink.text(locale)}
+                    component={
+                      sublink.type !== 'dropdown' ? 'a' : 'button'
+                    }
+                    href={
+                      sublink.type !== 'dropdown'
+                        ? sublink.href
+                        : undefined
+                    }
+                  />
+                ))}
+          </NavLink>
+        ))}
     </div>
   );
 };
